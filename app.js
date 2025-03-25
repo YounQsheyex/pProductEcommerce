8c0dc9a134fb52d213ad46b3ceb4a0c9f197fb20
YounQsheyex <younqsheyex@gmail.com>
2025-03-25 12:21:46 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const API_URL = "https://dummyjson.com/products";
+
+//selecting elements
+const eProduct = document.querySelector(".inner-product");
+//fetching the api url
+
+async function getProducts(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+  if (data.products.length > 0) {
+    displayProducts(data.products);
+  }
+}
+
+getProducts(API_URL);
+
+function displayProducts(product) {
+  product.forEach((product) => {
+    const innerProuduct = document.createElement("div");
+    innerProuduct.classList.add("product");
+    const { images, title, price, stock, rating } = product;
+    innerProuduct.innerHTML = ` 
+  <img src="${images[0]}" alt="${title}" />
+  <div>
+    <h3>${title} </h3>
+    <p class="price">$${price}</p>
+    <p class="stock">${stock} items left</p>
+    <p>Rating:${rating}</p>
+    </div>
+    `;
+    eProduct.appendChild(innerProuduct);
+  });
+}
