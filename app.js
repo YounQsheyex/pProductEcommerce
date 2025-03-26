95852d261fa8cf0be448c90e257890f2905e016b
YounQsheyex <younqsheyex@gmail.com>
2025-03-26 02:46:23 -0700

    updated code

--- app.js
+++ app.js
@@ -4,22 +4,22 @@ const API_URL = "https://dummyjson.com/products";
 const eProduct = document.querySelector(".inner-product");
 //fetching the api url
 
-async function getProducts(url) {
-  const res = await fetch(url);
+async function getProducts() {
+  const res = await fetch(API_URL);
   const data = await res.json();
-  if (data.products.length > 0) {
-    displayProducts(data.products);
-  }
+  const { products } = data;
+  displayProducts(products);
 }
 
-getProducts(API_URL);
+getProducts();
 
-function displayProducts(product) {
-  product.forEach((product) => {
-    const innerProuduct = document.createElement("div");
-    innerProuduct.classList.add("product");
+function displayProducts(products) {
+  products.forEach((product) => {
+    const productDiv = document.createElement("div");
+    // innerProuduct.classList.add("product");
+    productDiv.className = 'product'
     const { images, title, price, stock, rating } = product;
-    innerProuduct.innerHTML = ` 
+    productDiv.innerHTML = ` 
   <img src="${images[0]}" alt="${title}" />
   <div>
     <h3>${title} </h3>
@@ -28,6 +28,6 @@ function displayProducts(product) {
     <p class="rate">Rating:${rating}</p>
     </div>
     `;
-    eProduct.appendChild(innerProuduct);
+    eProduct.appendChild(productDiv);
   });
 }
